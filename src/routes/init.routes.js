e95ce7afffff5346f52f73bbc85fa63504be4327
NguyenChinh0207 <nchinh407@gmail.com>
2023-02-04 09:45:28 +0700

    update log

--- src/routes/init.routes.js
+++ src/routes/init.routes.js
@@ -1,16 +1,20 @@
-import adminRouter from './admin.js';
-import authRouter from './auth.js';
-import courseRouter from './course.js';
-import examRouter from './exam.js';
-import progressRouter from './progress.js';
-import wordRouter from './word.js';
+import adminRouter from "./admin.js";
+import authRouter from "./auth.js";
+import courseRouter from "./course.js";
+import examRouter from "./exam.js";
+import progressRouter from "./progress.js";
+import wordRouter from "./word.js";
 
 const initRoutes = (app) => {
-    app.use("/api/admin", adminRouter);
-    app.use("/api/auth", authRouter);
-    app.use("/api/courses", courseRouter);
-    app.use("/api/words", wordRouter)
-    app.use("/api/progress", progressRouter);
-    app.use("/api/exams", examRouter);
-}
+  app.use("/api/admin/test", (req, res) => {
+    console.log("admin endpoint is called");
+    // your endpoint logic here
+  });
+  app.use("/api/admin", adminRouter);
+  app.use("/api/auth", authRouter);
+  app.use("/api/courses", courseRouter);
+  app.use("/api/words", wordRouter);
+  app.use("/api/progress", progressRouter);
+  app.use("/api/exams", examRouter);
+};
 export default initRoutes;
